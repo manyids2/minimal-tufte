2dac19126707b1e1d571f2e26d9c1522a38f7831
manyids2 <arun.mukundan@gmail.com>
2024-01-28 14:41:00 +0100

    Copy button works!

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,45 @@ window
   .addEventListener("change", () => {
     setSyntaxDark(isDarkMode());
   });
+
+// https://codepen.io/bharatramnani94/post/copy-text-to-clipboard-using-vanilla-javascript
+function copyText(event) {
+  var btn = event.target;
+  var div = btn.parentElement.parentElement;
+  var code = div.querySelector("code");
+
+  var elements = code.getElementsByClassName("line");
+  var text = [];
+  if (elements) {
+    if (elements.length > 0) {
+      for (var i = 0; i < elements.length; i++) {
+        text.push(elements[i].innerText);
+      }
+      text = text.join("");
+    } else {
+      text = code.innerText;
+    }
+  }
+
+  var myTemporaryInputElement = document.createElement("textarea");
+  myTemporaryInputElement.type = "text";
+  myTemporaryInputElement.value = text;
+  document.body.appendChild(myTemporaryInputElement);
+
+  myTemporaryInputElement.select();
+  document.execCommand("Copy");
+  document.body.removeChild(myTemporaryInputElement);
+  console.log("copied:", text);
+
+  btn.classList.add("copied");
+  setTimeout(function() {
+    btn.classList.remove("copied");
+  }, 2000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  var elements = document.getElementsByClassName("copy-cmd");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", copyText);
+  }
+});
